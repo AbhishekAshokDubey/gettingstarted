e33c2bc89423be40eddc279b092696bad388dd7f
Abhishek Dubey <AbhishekAshokDubey@users.noreply.github.com>
2015-04-20 17:24:21 +0530

    Update readFile.py

--- python/readFile.py
+++ python/readFile.py
@@ -5,3 +5,20 @@ with open('abalone.data', 'rb') as f:
 
 # i th row can be read as data_list[i]
 # type(data_list[i]) is a 'list'
+###############################################################################
+
+lines = []
+with open('../kaggleDS/UCI/abalone.data', 'rt') as f:
+    for line in f.readlines():
+		try:			
+			l = line.strip().split(',')
+			lines.append((l))
+		except ValueError:
+			print('Error'.format(line))
+
+# i th row can be read as lines[i]
+# i th row and j th column can be read as lines[i][j]
+###############################################################################
+
+lines = open('../kaggleDS/UCI/abalone.data', 'rt').read().splitlines()
+# i th row, which is one string can be read as lines[i]
