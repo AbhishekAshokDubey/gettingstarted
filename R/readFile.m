4027f06dc85c5e8ffb076b706cfc68edefb29c43
Abhishek Dubey <AbhishekAshokDubey@users.noreply.github.com>
2015-04-20 14:41:18 +0530

    Update readFile.m

--- R/readFile.m
+++ R/readFile.m
@@ -3,4 +3,11 @@
 # Clear everything in your environment
 rm(list = ls());
 
-f1 = read.csv('../data/abalone.data', header = TRUE, sep = ",",na.strings = "-");
+# read the data file
+f1 = read.csv('../data/abalone.data', header = FALSE, sep = ",",na.strings = "-");
+# i th row can be referred as         f1[i,]
+# i th column can be referred as      f1[,i]
+
+f1 = read.table('../data/abalone.data', header = FALSE, sep = ",");
+# i th row can be referred as         f1[i,]
+# i th column can be referred as      f1[,i]
