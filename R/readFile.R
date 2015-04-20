2fe0a5fcbfe46d9b4fc5603a903497d256c27650
Abhishek Dubey <AbhishekAshokDubey@users.noreply.github.com>
2015-04-20 14:43:40 +0530

    Update readFile.R

--- R/readFile.R
+++ R/readFile.R
@@ -4,7 +4,7 @@
 rm(list = ls());
 
 # read the data file
-f1 = read.csv('../data/abalone.data', header = FALSE, sep = ",",na.strings = "-");
+f1 = read.csv('../data/abalone.data', header = FALSE, sep = ",", na.strings = "-");
 # i th row can be referred as         f1[i,]
 # i th column can be referred as      f1[,i]
 
