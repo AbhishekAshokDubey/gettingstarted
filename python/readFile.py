b93a1f97d2cc900fc0bb5e2ff4964d44b70b2635
Abhishek Dubey <AbhishekAshokDubey@users.noreply.github.com>
2015-04-20 16:34:02 +0530

    Update readFile.py

--- python/readFile.py
+++ python/readFile.py
@@ -1,1 +1,7 @@
+import csv
+with open('abalone.data', 'rb') as f:
+    reader = csv.reader(f)
+    data_list = list(reader)
 
+# i th row can be read as data_list[i]
+# type(data_list[i]) is a 'list'
